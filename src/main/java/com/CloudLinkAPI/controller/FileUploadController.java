f8c21568b660c59324f6b562a2472c08304e3c5c
rodrigovcf <rodrigo.rvcf@gmail.com>
2025-03-01 19:37:52 -0300

    Criação de um endpoint para fazer o upload

---
+++ src/main/java/com/CloudLinkAPI/controller/FileUploadController.java
@@ -0,0 +1,29 @@
+package com.CloudLinkAPI.controller;
+
+import com.CloudLinkAPI.services.AzureBlobStorageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/files")
+public class FileUploadController {
+
+    private final AzureBlobStorageService storageService;
+
+    public FileUploadController(AzureBlobStorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String fileUrl = storageService.uploadFile(file);
+            return ResponseEntity.ok(fileUrl);
+        } catch (IOException e) {
+            return ResponseEntity.status(500).body("Erro ao fazer upload do arquivo");
+        }
+    }
+}
