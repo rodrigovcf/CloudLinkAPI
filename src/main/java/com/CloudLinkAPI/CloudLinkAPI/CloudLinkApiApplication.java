b51edf0bfd47557c1e2349496af079854d0dd799
rodrigovcf <rodrigo.rvcf@gmail.com>
2025-03-01 17:13:36 -0300

    Iniciando projeto CloudLink API

---
+++ src/main/java/com/CloudLinkAPI/CloudLinkAPI/CloudLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.CloudLinkAPI.CloudLinkAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLinkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CloudLinkAPI/CloudLinkAPI/CloudLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CloudLinkAPI.CloudLinkAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudLinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
